add23eaeb32bdcff98c4a98105ffdcbda78c76f2
Giulio Calzolari <9049490+giuliocalzolari@users.noreply.github.com>
2021-03-05 16:10:59 +0100

    Create Route53.js

---
+++ Route53.js
@@ -0,0 +1,122 @@
+
+/**
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+	"use strict";
+
+	function AmazonAPINode(n) {
+		RED.nodes.createNode(this,n);
+		this.awsConfig = RED.nodes.getNode(n.aws);
+		this.region = n.region;
+		this.operation = n.operation;
+		this.name = n.name;
+		this.region = this.awsConfig.region;
+		this.accessKey = this.awsConfig.accessKey;
+		this.secretKey = this.awsConfig.secretKey;
+
+		var node = this;
+		var AWS = require("aws-sdk");
+		AWS.config.update({
+			accessKeyId: this.accessKey,
+			secretAccessKey: this.secretKey,
+			region: this.region
+		});
+		if (!AWS) {
+			node.warn("Missing AWS credentials");
+			return;
+		}
+
+        if (this.awsConfig.proxyRequired){
+            var proxy = require('proxy-agent');
+            AWS.config.update({
+                httpOptions: { agent: new proxy(this.awsConfig.proxy) }
+            });
+        }
+
+		var awsService = new AWS.Route53( { 'region': node.region } );
+
+		node.on("input", function(msg) {
+			node.sendMsg = function (err, data, msg) {
+				if (err) {
+				    node.status({fill:"red",shape:"ring",text:"error"});
+                    node.error("failed: " + err.toString(), msg);
+                    node.send([null, { err: err }]);
+    				return;
+				} else {
+				msg.payload = data;
+				node.status({});
+				}
+				node.send([msg,null]);
+			};
+
+			if (typeof service[node.operation] == "function"){
+				node.status({fill:"blue",shape:"dot",text:node.operation});
+				service[node.operation](awsService,msg,function(err,data){
+   				node.sendMsg(err, data, msg);
+   			});
+			} else {
+				node.error("failed: Operation node defined - "+node.operation);
+			}
+
+		});
+		var copyArg=function(src,arg,out,outArg,isObject){
+			var tmpValue=src[arg];
+			outArg = (typeof outArg !== 'undefined') ? outArg : arg;
+
+			if (typeof src[arg] !== 'undefined'){
+				if (isObject && typeof src[arg]=="string" && src[arg] != "") {
+					tmpValue=JSON.parse(src[arg]);
+				}
+				out[outArg]=tmpValue;
+			}
+                        //AWS API takes 'Payload' not 'payload' (see Lambda)
+                        if (arg=="Payload" && typeof tmpValue == 'undefined'){
+                                out[arg]=src["payload"];
+                        }
+
+		}
+
+		var service={};
+
+		service.ChangeResourceRecordSets=function(svc,msg,cb){
+			var params={};
+			copyArg(n,"ChangeBatch",params,undefined,true);
+			copyArg(msg,"ChangeBatch",params,undefined,true);
+			copyArg(msg,"HostedZoneId",params,undefined,false);
+
+			svc.changeResourceRecordSets(params,cb);
+		}
+
+
+		service.ListResourceRecordSets=function(svc,msg,cb){
+			var params={};
+
+			copyArg(n,"HostedZoneId",params,undefined,false);
+			copyArg(msg,"HostedZoneId",params,undefined,false);
+			copyArg(msg,"MaxItems",params,undefined,false);
+			copyArg(msg,"StartRecordIdentifier",params,undefined,false);
+			copyArg(msg,"StartRecordName",params,undefined,false);
+			copyArg(msg,"StartRecordType",params,undefined,false);
+
+			svc.listResourceRecordSets(params,cb);
+		}
+
+
+
+	}
+	RED.nodes.registerType("AWS Route53", AmazonAPINode);
+
+};
